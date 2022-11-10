4bbfa8e9af3c5caf358066fbb0001d60d8add16d
Lijiehui <2453133813@qq.com>
2022-11-10 18:42:00 +0800

    完成了商品列表的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
-	<view>
-		这是商品列表
+	<view class="goods-list-view">
+		<view class="goods-list">
+			<view v-for="(goods,i) in goodsList" :key="i" @click="goToDetail(goods.goods_id)">
+				<my-goods :goods="goods"></my-goods>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,71 @@
 	export default {
 		data() {
 			return {
-
+				queryObj: {
+					query: "",
+					cat_id: "",
+					pagenum: 1,
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isLoading: false
 			};
+		},
+		onLoad(options) {
+			this.queryObj.query = options.query || ''
+			this.queryObj.cat_id = options.cid || ''
+			console.log("this.queryObj.query=>", this.queryObj.query);
+			console.log("this.queryObj.cid=>", this.queryObj.cid);
+			this.getGoodsList()
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isLoading = true
+				console.log("正在获取数据");
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isLoading = false
+				cb && cb()
+				if (res.meta.status !== 200) {
+					return uni.$showMsg()
+				}
+
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+				console.log("this.goodsList=>", this.goodsList);
+				console.log("this.total=>", this.total);
+			},
+			goToDetail(id) {
+				// console.log(id);
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + id
+				})
+			},
+		},
+
+		onReachBottom() {
+			// 上拉加载数据请求完
+			if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) {
+				return uni.$showMsg("没有更多了！")
+			}
+			// 上拉加载更多
+			if (this.isLoading) {
+				return
+			}
+			this.queryObj.pagenum++
+			this.getGoodsList()
+		},
+		onPullDownRefresh() {
+			this.queryObj.pagenum = 1
+			this.total = 0
+			this.isLoading = false
+			this.goodsList = []
+
+			this.getGoodsList(() => {
+				uni.stopPullDownRefresh()
+			})
 		}
 	}
 </script>
---
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="">
+		<view class="goods-item">
+			<view class="goods-item-left">
+				<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+			</view>
+			<view class="goods-item-right">
+				<view class="goods-name">{{goods.goods_name}}</view>
+				<view class="goods-info-box">
+					<view class="goods-price">￥{{goods.goods_price + ".00"}}</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+<script>
+	export default {
+		props: {
+			goods: {
+				type: Object,
+				default: {}
+			}
+		},
+		data() {
+			return {
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+			}
+		},
+
+	}
+</script>
+<style lang="scss" scoped>
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
+
+		.goods-item-left {
+			.goods-pic {
+				display: block;
+				width: 100px;
+				height: 100px;
+				margin-right: 10px;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-info-box {
+				.goods-price {
+					color: #C00000;
+					font-size: 16px;
+				}
+			}
+		}
+	}
+</style>
